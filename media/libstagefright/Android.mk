3c1fb3df1f0f8b05973d6ec0c173a1ca636fe992
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-03 02:45:19 +0000

    exynos4: libstragefright: add support for samsung colorformat/omx/mfc
    Source:
    http://git.insignal.co.kr/samsung/exynos/android/platform/frameworks/av/commit/?h=exynos-jb&id=1614612f7ca2a00473d202dbedcb135fadc608ad
    
    Conflicts:
    
    	media/libstagefright/ACodec.cpp
    	media/libstagefright/OMXCodec.cpp
    
    cherry pick from 906eba9f
    
    Change-Id: I1c174f8e9fa9bd3ed16a0399f070b6680f6a331c
    
    Conflicts:
    	media/libstagefright/Android.mk
    	media/libstagefright/OMXCodec.cpp

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -156,6 +156,15 @@ LOCAL_CLANG := true
 # FFMPEG plugin
 LOCAL_C_INCLUDES += $(TOP)/external/stagefright-plugins/include
 
+ifeq ($(BOARD_USE_SAMSUNG_COLORFORMAT), true)
+LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
+
+# Include native color format header path
+LOCAL_C_INCLUDES += \
+	$(TOP)/hardware/samsung/exynos4/hal/include \
+	$(TOP)/hardware/samsung/exynos4/include
+endif
+
 LOCAL_MODULE:= libstagefright
 
 LOCAL_MODULE_TAGS := optional
